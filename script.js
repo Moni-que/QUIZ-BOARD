c7277737b4f4e8784b0766bc4fa986a013800b5a
Moni-que <monicahjustus@gmail.com>
2022-02-06 23:49:29 +0300

    add if ...else statement to shore score and statement

--- script.js
+++ script.js
@@ -7,14 +7,25 @@ function check(){
     var q4=document.quiz.Question4.value;
     var q5=document.quiz.Question5.value;
     var lastparagraph=document.getElementById('lastparagraph');
+    var quiz=document.getElementById("quiz");
 
-    //writing if statements
+    //writing if statements to display answers
     if (q1=="A tool that programmers use to record the output of their javascript programs."){a++}
     if (q2=="none of the choices"){a++}
     if (q3=="kiswahili"){a++}
     if (q4=="looping"){a++}
     if (q5=="iLoveMe"){a++}
 
-        lastparagraph.textContent=`${a}`;
+    //making the answer to display without the question
+    quiz.style.display="none"
+
+
+        if(a>=4){
+            lastparagraph.textContent=`Your score is ${a}.Excellent!`;
+        
+        }else {
+            lastparagraph.textContent=`Your score is ${a}.poor`;
+        }
+
 
 }
