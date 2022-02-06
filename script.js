993b5a0202b010ba7af83b608f8027e9edd407b8
Moni-que <monicahjustus@gmail.com>
2022-02-06 16:01:31 +0300

    add a function check with var a=0 and if statement of if q1==a++

---
+++ script.js
@@ -0,0 +1,7 @@
+function check(){
+    var a=0
+    var q1=document.quiz.Question1.value;
+    if (q1=="A tool that programmers use to record the output of their javascript programs."){
+        a++}
+        document.write(a);
+    }
