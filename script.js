04b89c9db4e1cb25ff9614ea60874af0b279d5cc
Moni-que <monicahjustus@gmail.com>
2022-02-06 17:55:13 +0300

    add a function check with var a=0 and if statements of if q1,q2,q3,q4,and q5== a++

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+//writing a fuction with a variable
 function check(){
     var a=0
     var q1=document.quiz.Question1.value;
-    if (q1=="A tool that programmers use to record the output of their javascript programs."){
-        a++}
+    var q2=document.quiz.Question2.value;
+    var q3=document.quiz.Question3.value;
+    var q4=document.quiz.Question4.value;
+    var q5=document.quiz.Question5.value;
+
+    //writing if statements
+    if (q1=="A tool that programmers use to record the output of their javascript programs."){a++}
+    if (q2=="none of the choices"){a++}
+    if (q3=="kiswahili"){a++}
+    if (q4=="looping"){a++}
+    if (q5=="iLoveMe"){a++}
+
         document.write(a);
-    }
+
+}
